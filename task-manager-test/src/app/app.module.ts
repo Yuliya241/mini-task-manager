bd435729b0ba5d29d9811381a9de20fd1ad2e94c
Yuliya Shelenhouskaya <yulchik241@mail.ru>
2024-03-08 23:42:25 +0300

    feat: install all necessary packages

---
+++ task-manager-test/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { isDevMode, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    StoreModule.forRoot({}, {}),
+    EffectsModule.forRoot([]),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() }),
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule { }
